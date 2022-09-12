56921489dae9f6878aac214eebd8b7eba6a551bd
Saood <saood.imran@ontariotechu.net>
2022-09-11 22:01:21 -0400

    File name changes

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -5,18 +5,19 @@
     <div class="flex items-center justify-center">
       <div class="w-full max-w-md">
         <form class="text-left tbg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4 mt-4 mt-40">
-          <img class="mb-8 mx-auto" src="../assets/FUCKDISCORD.png">
+          <img class="mb-8 mx-auto" src="../assets/NOTDISCORD.png">
           <!-- @csrf -->
             <div class="mb-4">
               <label class="block text-gray-700 text-sm font-normal mb-2" for="email">Email</label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" v-model="email" required autofocus placeholder="someone@example.com"/>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" v-model="email" required autofocus placeholder="someone@example.com"  @keyup.enter="submitForm(email,password)"/>
             </div>
 
             <div class="mb-5">
               <label class="block text-gray-700 text-sm font-normal mb-2" for="password">Password</label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" v-model="password" required autofocus placeholder="Password" />
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" v-model="password" required autofocus placeholder="Password" @keyup.enter="submitForm(email,password)" />
             </div>
             <h1 v-if="incorrectDetails" class="block text-gray-700 text-sm font-normal mb-2">Invalid username or password.</h1>
+            <h1 v-if="EmptyInput" class="block text-gray-700 text-sm font-normal mb-2">Please fill the required fields.</h1>
             <div class="flex items-center justify-between">
               <button class="px-4 py-2 rounded text-white inline-block shadow-lg bg-[#facc15] hover:bg-[#eab308]" type="button" v-on:click="submitForm(email, password)">Login</button>
             </div>
@@ -44,7 +45,8 @@ export default {
         email: "",
         password: ""
       },
-      incorrectDetails: false
+      incorrectDetails: false,
+      EmptyInput: false,
 
     }
   },
@@ -59,34 +61,38 @@ export default {
       this.loginDetails.email = email
       this.loginDetails.password = password
 
+      if (this.loginDetails.email == null || this.loginDetails.password == null){
+        this.EmptyInput = true
+      }
+      else{
+        axios.post('https://concord.dafoe.me/auth/login',this.loginDetails, {headers: {
+          'Content-type': 'application/json',
+        }, withCredentials: true}).then((res) =>{
+          console.log("Submitted successfully, success code: " + res.status)
+          console.log(res)
+          if (res.data.status == 200){
+            var id = JSON.parse(res.data.msg)["id"]
+            //Either read cookiees or use mapmutations to store user and token values to enable login as default state (vuex storage)
+            store.commit("setUser", id)
 
+            var jwtTTL = JSON.parse(res.data.msg)["jwtttl"]
+            var newTTL = jwtTTL + Math.round(Date.now() / 1000)
+            store.commit("setJwtTokenTTL", newTTL)
 
-      axios.post('https://concord.dafoe.me/auth/login',this.loginDetails, {headers: {
-        'Content-type': 'application/json',
-      }, withCredentials: true}).then((res) =>{
-        console.log("Submitted successfully, success code: " + res.status)
-        console.log(res)
-        if (res.data.status == 200){
-          var id = JSON.parse(res.data.msg)["id"]
-          //Either read cookiees or use mapmutations to store user and token values to enable login as default state (vuex storage)
-          store.commit("setUser", id)
+            this.$router.push('https://127.0.0.1:8080/home')
+          }
+          else{
+            this.incorrectDetails = true
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          console.log("Failed to submit data with error code: " + error.status)
+        }).finally(() =>{
+          
+        });
+      }
 
-          var jwtTTL = JSON.parse(res.data.msg)["jwtttl"]
-          var newTTL = jwtTTL + Math.round(Date.now() / 1000)
-          store.commit("setJwtTokenTTL", newTTL)
-
-          this.$router.push('https://127.0.0.1:8080/home')
-        }
-        else{
-          this.incorrectDetails = true
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-        console.log("Failed to submit data with error code: " + error.status)
-      }).finally(() =>{
-        
-      });
     }
   }
 }
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -6,23 +6,24 @@
       <img src="../assets/LeftWing.png">
       <div class="w-full max-w-md mt-20">
         <form class="text-left bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4 mt-40 border">
-            <img class="mb-8 mx-auto" src="../assets/FUCKDISCORD.png">
+            <img class="mb-8 mx-auto" src="../assets/NOTDISCORD.png">
           <!-- @csrf -->
             <div class="mb-4">
               <label class="block text-gray-700 text-sm font-normal mb-2" for="email">Email</label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" v-model="email" required autofocus placeholder="someone@example.com"/>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" type="email" v-model="email" required autofocus placeholder="someone@example.com" @keyup.enter="submitForm(email,username,password)" />
             </div>
 
             <div class="mb-6">
               <label class="block text-gray-700 text-sm font-normal mb-2" for="username">Username</label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="username" type="username" v-model="username" required autofocus placeholder="Username"/>
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="username" type="username" v-model="username" required autofocus placeholder="Username" @keyup.enter="submitForm(email,username,password)" />
             </div>
 
             <div class="mb-5">
               <label class="block text-gray-700 text-sm font-normal mb-2" for="password">Password</label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" v-model="password" required autofocus placeholder="Password" />
+              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" v-model="password" required autofocus placeholder="Password" @keyup.enter="submitForm(email,username,password)" />
             </div>
             <h1 v-if="registerFailure" class="block text-gray-700 text-sm font-normal mb-2">{{ serverResponse }}</h1>
+            <h1 v-if="EmptyInput" class="block text-gray-700 text-sm font-normal mb-2">Please fill the required fields.</h1>
             <div class="flex items-center justify-between">
               <button class="px-4 py-2 rounded text-white inline-block shadow-lg bg-[#facc15] hover:bg-[#eab308]" type="button" v-on:click="submitForm(email, username, password)" >Register</button>
             </div>
@@ -50,10 +51,11 @@ export default {
       registrationDetails: {
         email: "",
         username: "",
-        password: ""
+        password: "",
       },
       registerFailure: false,
-      serverResponse: ""
+      serverResponse: "",
+      EmptyInput: false,
       
     }
   },
@@ -67,35 +69,42 @@ export default {
       this.registrationDetails.username = username
       this.registrationDetails.password = password
 
-      axios.post('https://concord.dafoe.me/auth/register',this.registrationDetails, {headers: {
-        'Content-type': 'application/json',
-      }, withCredentials: true}).then((res) =>{
-        console.log("Submitted successfully, success code: " + res.status)
-        console.log(res)
+      if (this.registrationDetails.email == null || this.registrationDetails.password == null || this.registrationDetails.username == null){
+        console.log(this.registrationDetails)
+        this.EmptyInput = true
+      }
+      else{
+        console.log(this.registrationDetails)
+        axios.post('https://concord.dafoe.me/auth/register',this.registrationDetails, {headers: {
+          'Content-type': 'application/json',
+        }, withCredentials: true}).then((res) =>{
+          console.log("Submitted successfully, success code: " + res.status)
+          console.log(res)
 
-        if (res.data.status == 200){
-          var id = JSON.parse(res.data.msg)["id"]
-          console.log(id)
-          store.commit("setUser", id)
-          var jwtTTL = JSON.parse(res.data.msg)["jwtttl"]
-          var newTTL = jwtTTL + Math.round(Date.now() / 1000)
-          store.commit("setJwtTokenTTL", newTTL)
+          if (res.data.status == 200){
+            var id = JSON.parse(res.data.msg)["id"]
+            console.log(id)
+            store.commit("setUser", id)
+            var jwtTTL = JSON.parse(res.data.msg)["jwtttl"]
+            var newTTL = jwtTTL + Math.round(Date.now() / 1000)
+            store.commit("setJwtTokenTTL", newTTL)
 
 
-          this.$router.push('https://127.0.0.1:8080/home')
-        }
-        else{
-          console.log(res.data.msg)
-          this.serverResponse = res.data.msg
-          this.registerFailure = true
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-        console.log("Failed to submit data with error code: " + error.status)
-      }).finally(() =>{
-        console.log("Refresh to try again.")
-      });
+            this.$router.push('https://127.0.0.1:8080/home')
+          }
+          else{
+            console.log(res.data.msg)
+            this.serverResponse = res.data.msg
+            this.registerFailure = true
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          console.log("Failed to submit data with error code: " + error.status)
+        }).finally(() =>{
+          console.log("Refresh to try again.")
+        });
+      }
     }
   }
 }
